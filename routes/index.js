f04edb448145680165398eb1dee93d5f84f26205
tiffanyrudd <you@example.com>
2018-12-11 15:55:33 -0700

    updated code

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var router = require("express").Router();
+var apiRoutes = require("./api");
+var viewRoutes = require("./view");
+
+router.use("/api", apiRoutes);
+router.use("/", viewRoutes);
+
+module.exports = router;
+
